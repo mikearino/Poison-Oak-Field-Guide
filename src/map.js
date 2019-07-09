c63d512bbb46545441bd6a2524be91d104dbdef3
Garland Lai and Nick Ramsey <nick@>
2019-07-09 09:36:53 -0700

    add map api

---
+++ src/map.js
@@ -0,0 +1,17 @@
+
+// var myLatLng = {lat: 45.5155, lng: -122.6793};
+// var map;
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById('map'), {
+//     center: {lat: 45.5155, lng: -122.6793},
+//     zoom: 10
+//   });
+//
+//   var maker = new google.maps.Marker({
+//     position: myLatLng,
+//     map: map,
+//     title: 'Poison Oak'
+//   });
+// }
+// src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCbFgi-9P2b1_bLz_pm3FKLDJrEN6rRwac&callback=initMap"
+// async defer
