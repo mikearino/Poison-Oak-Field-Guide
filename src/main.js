8caa3d692f648ca29a5955a162538201bf4f6cce
Garland Lai and Nick Ramsey <garland@>
2019-07-09 10:59:58 -0700

    allow for multiple pins

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { sum } from './sample-class.js';
+// import { mapAPI } from './map.js';
 import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
@@ -6,4 +6,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 $(document).ready(function() {
 
+//   var myLatLng = {lat: 45.5155, lng: -122.6793};
+//   var map;
+//   function initMap() {
+//     map = new google.maps.Map(document.getElementById('map'), {
+//       center: {lat: 45.5155, lng: -122.6793},
+//       zoom: 10
+//     });
+//
+//     var maker = new google.maps.Marker({
+//       position: myLatLng,
+//       map: map,
+//       title: 'Poison Oak'
+//     });
+//   $('#map').append(map);
+//   }
 });
+
+
+
+
+
+
+//   let mapAPI = new MapAPI();
+//   let promise = mapAPI.findPoisonOak();
+//   $("#api").append(map);
--- src/map.js
+++ src/map.js
@@ -1,4 +1,4 @@
-
+//
 // var myLatLng = {lat: 45.5155, lng: -122.6793};
 // var map;
 // function initMap() {
@@ -13,5 +13,35 @@
 //     title: 'Poison Oak'
 //   });
 // }
-// src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCbFgi-9P2b1_bLz_pm3FKLDJrEN6rRwac&callback=initMap"
+// // src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCbFgi-9P2b1_bLz_pm3FKLDJrEN6rRwac&callback=initMap"
 // async defer
+
+
+// export class MapAPI {
+//   findPoisonOak() {
+//     return new Promise(function(resolve, reject) {
+//       let myLatLng = {lat: 45.5155, lng: -122.6793};
+//       let map;
+//       function initMap() {
+//         map = new google.maps.Map(document.getElementById('map'), {
+//           center: {lat: 45.5155, lng: -122.6793},
+//           zoom: 10
+//         });
+//         let maker = new google.maps.Marker({
+//           position: myLatLng,
+//           map: map,
+//           title: 'Poison Oak'
+//       let request = new XMLHttpRequest();
+//       let url = `https://maps.googleapis.com/maps/api/js?key=AIzaSyCbFgi-9P2b1_bLz_pm3FKLDJrEN6rRwac&callback=initMap`;
+//       request.onload = function () {
+//         if (this.status === 200) {
+//           resolve(request.responseText);
+//         } else {
+//           reject(Error(request.statusText));
+//         }
+//       }
+//       request.open("GET", url, true);
+//       request.send();
+//     });
+//   }
+// }
